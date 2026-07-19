ead44b6d4036db2548fdc2b6922e81f318c8d804
Sakthi Sree Raja <sakthisreeraja@gmail.com>
2026-07-19 18:59:10 +0530

    Initialize Spring Boot backend project

---
+++ ReserveX_Backend/src/main/java/com/sakthi/reservexbackend/ReserveXBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sakthi.reservexbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReserveXBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReserveXBackendApplication.class, args);
+    }
+
+}
---
+++ ReserveX_Backend/src/test/java/com/sakthi/reservexbackend/ReserveXBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sakthi.reservexbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReserveXBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
