3eabd18fb2cbf6ba76512984c23886c6e14d060c
Sakthi Sree Raja <sakthisreeraja@gmail.com>
2026-07-19 21:23:07 +0530

    Frontend and Backend setup completed

---
+++ ReserveX_Frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <p className="text-red-500 text-3xl font-bold">
+    Hello World!!
+</p>
+    </>
+
+  )
+}
+
+export default App
